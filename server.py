8264087b7b783e255f7eb3286a5af93662a44a89
One <idohaolam@gmail.com>
2023-02-01 12:31:43 +0200

    Added server Added index blueprint

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from blueprints import index
+
+
+def create_app() -> Flask:
+    """Returns a new app."""
+
+    app = Flask(__name__)
+    app.register_blueprint(index)
+
+    return app
+
+
+def start_server() -> None:
+    """Starts a server"""
+
+    create_app().run('127.0.0.1', 8080, debug=True)
