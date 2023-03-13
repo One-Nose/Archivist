72403a8d266cb3c49d80da230bd6beea1828c12a
One <idohaolam@gmail.com>
2023-03-13 15:19:34 +0200

    Added database connecting to main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""The root file of the program"""
+from database import Database
 from server import start_server
 
 if __name__ == '__main__':
+    database = Database({'connect': {'user': 'root', 'password': 'root', 'database': 'archivist'}})
+    print(database.get_tables())
+
     start_server()
