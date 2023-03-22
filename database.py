39fd118d06e1f7a69891ccab9c5734563a4dedc9
One <idohaolam@gmail.com>
2023-03-22 11:59:31 +0200

    Added Database.create_table() Removed Database.get_tables() Added Database.create_tables(), unimplemented

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 """Access the database and make queries"""
+from collections.abc import Sequence
+
 from MySQLdb import connect, Connection
 from MySQLdb.cursors import Cursor
 
@@ -12,7 +14,14 @@ class Database:
         self.database = connect(**config.get('connect', {}))
         self.cursor = self.database.cursor()
 
-    def get_tables(self) -> list:
-        """:return: A list of the tables in the database"""
-        self.cursor.execute("""SHOW TABLES""")
-        return self.cursor.fetchone()
+    def create_table(self, table: str, columns: Sequence[tuple[str, str]]) -> None:
+        """
+        Creates a table
+        :param columns: The columns of the table. Each column is in the form of (name, type)
+        :param table: The name of the table
+        """
+        self.cursor.execute(f"""CREATE TABLE %s ({'%s %s, ' * len(columns)});""", (table, *[*columns]))
+
+    def create_tables(self) -> None:
+        """Creates the required tables for the database"""
+        # TODO: Implement function
