4e430cdedd20040e4a5a6c735c00f3bab2417c5a
One Nose <idohaolam@gmail.com>
2023-03-29 00:21:08 +0300

    Commented unused import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from json import load
 
 from database import Database
-from server import start_server
+# from server import start_server
 
 if __name__ == '__main__':
     with open('config.json') as file:
