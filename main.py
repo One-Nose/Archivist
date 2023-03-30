248ba4936726b540f98c5b8dfe5526ca438fee65
One Nose <idohaolam@gmail.com>
2023-03-30 18:05:33 +0300

    Explicitated config.json encoding

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from archive import Archive
 # from server import start_server
 
 if __name__ == '__main__':
-    with open('config.json') as file:
+    with open('config.json', encoding='utf-8') as file:
         config = load(file)
 
     archive = Archive(config)
