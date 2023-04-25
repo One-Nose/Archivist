1c60d1a234473df4e1144925d2656f8924d2c5ce
One Nose <idohaolam@gmail.com>
2023-04-25 17:13:33 +0300

    Removed commented start_server() in main.py

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from json import load
 
 from src.archive import Archive
 
-# from server import start_server
-
 if __name__ == '__main__':
     with open('config.json', encoding='utf-8') as file:
         config = load(file)
 
     archive = Archive(config)
-
-    # start_server()
