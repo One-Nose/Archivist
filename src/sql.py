ee6c33fa1ca3e6179d134ecafa5e578ead8f789b
One Nose <idohaolam@gmail.com>
2023-04-27 10:56:50 +0300

    Added Use statement

--- src/sql.py
+++ src/sql.py
@@ -3,3 +3,17 @@
 
 class Statement:
     """Interface for representing an SQL statement"""
+
+
+class Use(Statement):
+    """This statement sets the current database to a chosen one"""
+
+    _database: str
+
+    def __init__(self, database: str) -> None:
+        super().__init__()
+
+        self._database = database
+
+    def __repr__(self) -> str:
+        return f'USE {self._database}'
