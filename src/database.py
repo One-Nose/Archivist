74849d91d4df132c1515609ad3de85fac289387e
One Nose <idohaolam@gmail.com>
2023-04-28 17:11:27 +0300

    Database now inherits from dict Database now updates itself with the tables in Database.__init__()

--- src/database.py
+++ src/database.py
@@ -15,23 +15,6 @@ class Column:
         return f'{self._name} {self._type}'
 
 
-class Database:
-    """Represents an SQL database"""
-
-    _name: str
-    _password: str
-    _username: str
-
-    def __init__(self, username: str, password: str, database: str) -> None:
-        self._username = username
-        self._password = password
-        self._name = database
-
-    def use(self) -> str:
-        """Retuns a USE statement"""
-        return f'USE {self._name}'
-
-
 class Table(dict[str, Column]):
     """Represents an SQL table"""
 
@@ -50,3 +33,78 @@ class Table(dict[str, Column]):
         """Returns a CREATE TABLE statement"""
 
         return f'CREATE TABLE {self.name} ({", ".join(map(str, self.values()))})'
+
+
+class Database(dict[str, Table]):
+    """Represents an SQL database"""
+
+    _name: str
+    _password: str
+    _username: str
+
+    def __init__(self, username: str, password: str, database: str) -> None:
+        self._username = username
+        self._password = password
+        self._name = database
+
+        self.update(
+            {
+                table.name: table
+                for table in (
+                    Table(
+                        'categories',
+                        id='INT AUTO_INCREMENT PRIMARY KEY',
+                        name='VARCHAR(255) NOT NULL',
+                    ),
+                    Table(
+                        'declarations',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        document='INT UNSIGNED NOT NULL',
+                        category='INT NOT NULL',
+                    ),
+                    Table(
+                        'descriptions',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        declaration='INT UNSIGNED NOT NULL',
+                        description='TEXT NOT NULL',
+                    ),
+                    Table(
+                        'documents',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        name='VARCHAR(255) NOT NULL',
+                    ),
+                    Table(
+                        'elements',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        category='INT NOT NULL',
+                    ),
+                    Table(
+                        'properties',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        parent='INT NOT NULL',
+                        name='VARCHAR(255) NOT NULL',
+                        category='INT NOT NULL',
+                    ),
+                    Table(
+                        'property_declarations',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        declaration='INT UNSIGNED NOT NULL',
+                        property='INT UNSIGNED NOT NULL',
+                        value='INT UNSIGNED NOT NULL',
+                    ),
+                    Table(
+                        'rules',
+                        id='INT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+                        category='INT NOT NULL',
+                        property1='INT UNSIGNED NOT NULL',
+                        subproperty1='INT UNSIGNED NOT NULL',
+                        property2='INT UNSIGNED NOT NULL',
+                        subproperty2='INT UNSIGNED NOT NULL',
+                    ),
+                )
+            }
+        )
+
+    def use(self) -> str:
+        """Retuns a USE statement"""
+        return f'USE {self._name}'
