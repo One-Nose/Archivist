501001f5a95b57ecebefc442659a1728ff8ff530
One Nose <idohaolam@gmail.com>
2023-05-02 19:56:51 +0300

    test(column-types): revamp tests
    Add TestColumnType.Empty class for column type tests.
    Use fixtures for common tasks.
    Merge multiple tests of the same method into classes.
    Rename tests to be more consistent. Each test function is now named
    test_<tested_function> and each test class is
    Test<CamelCaseTestedFunction>.
    Remove classes declared inside function. Private attributes are now
    tested using getattr().
    Rename test-strings to more meaningful names.

--- tests/test_column_types.py
+++ tests/test_column_types.py
@@ -1,68 +1,71 @@
+from pytest import fixture
+
 from src.column_types import ColumnType, IntColumnType, StrColumnType
 
 
 class TestColumnType:
-    def test_init(self):
-        assert ColumnType(123).value == 123
+    class Empty(ColumnType):
+        _SQL = 'EMPTY'
 
-    def test_str_int(self):
-        assert str(ColumnType(123)) == '123'
+    @fixture
+    def column_type(self):
+        return ColumnType(123)
 
-    def test_str_str(self):
-        assert str(ColumnType('hello')) == 'hello'
+    @fixture
+    def empty(self):
+        return self.Empty(123)
 
-    def test__sql(self):
-        class GenericColumnType(ColumnType):
-            _SQL = 'GENERIC'
+    def test___init__(self, column_type: ColumnType):
+        assert column_type.value == 123
 
-            def test_sql(self):
-                return self._sql()
+    class Test__str__:
+        def test_int(self, column_type: ColumnType):
+            assert str(column_type) == '123'
 
-        assert GenericColumnType(123).test_sql() == 'GENERIC'
+        def test_str(self):
+            assert str(ColumnType('value')) == 'value'
 
-    def test_sql(self):
-        class GenericColumnType(ColumnType):
-            _SQL = 'GENERIC'
+    def test__sql(self, empty: Empty):
+        assert getattr(empty, '_sql')() == 'EMPTY'
 
-        assert GenericColumnType(123).sql() == 'GENERIC NOT NULL'
+    def test_sql(self, empty: Empty):
+        assert empty.sql() == 'EMPTY NOT NULL'
 
 
 class TestIntColumnType:
-    def test_init(self):
-        assert IntColumnType(123).value == 123
+    @fixture
+    def column_type(self):
+        return IntColumnType(123)
 
-    def test_eq_true(self):
-        assert IntColumnType(123) == IntColumnType(123)
+    def test___init__(self, column_type: IntColumnType):
+        assert column_type.value == 123
 
-    def test_eq_wrong_id(self):
-        assert IntColumnType(123) != IntColumnType(456)
+    class Test__eq__:
+        def test_true(self, column_type: IntColumnType):
+            assert column_type == IntColumnType(123)
 
-    def test_eq_wrong_type(self):
-        assert IntColumnType(123) != ColumnType(123)
+        def test_wrong_value(self, column_type: IntColumnType):
+            assert column_type != IntColumnType(456)
 
-    def test_sql_signed(self):
-        class SignedIntColumnType(IntColumnType):
+        def test_wrong_type(self, column_type: IntColumnType):
+            assert column_type != ColumnType(123)
+
+    class Test_sql:
+        class Signed(IntColumnType):
             _SIGNED = True
 
-            def test_sql(self):
-                return self._sql()
+        def test_signed(self):
+            assert getattr(self.Signed(123), '_sql')() == 'INT'
 
-        assert SignedIntColumnType(123).test_sql() == 'INT'
+        def test_unsigned(self, column_type: IntColumnType):
+            assert getattr(column_type, '_sql')() == 'INT UNSIGNED'
 
-    def test_sql_unsigned(self):
-        class UnsignedIntColumnType(IntColumnType):
-            def test_sql(self):
-                return self._sql()
-
-        assert UnsignedIntColumnType(123).test_sql() == 'INT UNSIGNED'
-
-    def test_primary_key(self):
+    def test_primary_key(self, column_type: IntColumnType):
         assert (
-            IntColumnType(123).primary_key().sql()
-            == 'INT UNSIGNED AUTO_INCREMENT PRIMARY KEY'
+            column_type.primary_key().sql() == 'INT UNSIGNED AUTO_INCREMENT PRIMARY KEY'
         )
 
 
 class TestStrColumnType:
-    def test_init(self):
-        assert StrColumnType('hello').value == 'hello'
+    def test___init__(self):
+        assert StrColumnType('type').value == 'type'
