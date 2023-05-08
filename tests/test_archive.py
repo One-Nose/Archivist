8c70c122a1039d2f358a5b9f18f9cb7d7977c1e3
One Nose <idohaolam@gmail.com>
2023-05-08 16:01:43 +0300

    test(archive): remove Archive.__init__() testing

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -2,8 +2,7 @@ from json import load
 
 from pytest import fixture
 
-from src.analyzer import Analyzer
-from src.archive import Archive, ArchiveConfig, Category
+from src.archive import Archive, ArchiveConfig
 
 
 class TestArchive:
@@ -18,23 +17,5 @@ class TestArchive:
         yield archive
         archive.close()
 
-    class Test__init__:
-        def test_database(self, archive: Archive):
-            assert getattr(getattr(archive, '_database'), '_connection').ping() is None
-
-        def test_analyzer(self, archive: Archive):
-            analyzer = getattr(archive, '_analyzer')
-            assert (type(analyzer), getattr(analyzer, '_archive')) == (
-                Analyzer,
-                archive,
-            )
-
-        def test_greater(self, archive: Archive):
-            assert (type(archive.greater), archive.greater.id.value) == (Category, -1)
-
-        def test_less(self, archive: Archive):
-            assert (type(archive.less), archive.less.id.value) == (Category, -2)
-
     def test___repr__(self, archive: Archive, config: ArchiveConfig):
         assert repr(archive) == f'Archive({repr(config["connect"]["database"])})'
-
