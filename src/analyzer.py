28527257ec5c40d701eedf52a3b1b817fb32b7ec
One Nose <idohaolam@gmail.com>
2023-05-09 19:51:47 +0300

    refactor(analyzer): remove unused function
    Remove Analyzer.analyze_declaration()

--- src/analyzer.py
+++ src/analyzer.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .archive import Archive, Declaration
+    from .archive import Archive
 
 
 class Analyzer:
@@ -18,9 +18,3 @@ class Analyzer:
         :param archive: The archive to analyze
         """
         self._archive = archive
-
-    def analyze_declaration(self, declaration: Declaration) -> None:
-        """
-        Analyzes a declaration and saves the result in the database
-        :param declaration: The declaration to analyze
-        """
