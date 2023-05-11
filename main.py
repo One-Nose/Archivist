85af16ae7322ad8435ef25435ac3b7d458e63369
One Nose <idohaolam@gmail.com>
2023-05-11 20:49:00 +0300

    refactor(main): add a try/finally clause
    This is to allow me to view the changes on the database even if there
    was an error.

--- main.py
+++ main.py
@@ -10,27 +10,29 @@ if __name__ == '__main__':
     archive = Archive(config)
     archive.reset()
 
-    period = archive.new_category('Period')
-    beginning = period.new_property('Beginning')
-    end = period.new_property('End')
-    archive.add_order_rule(end, beginning)
+    try:
+        period = archive.new_category('Period')
+        beginning = period.new_property('Beginning')
+        end = period.new_property('End')
+        archive.add_order_rule(end, beginning)
 
-    episode4 = archive.new_document('Star Wars Episode IV: A New Hope')
+        episode4 = archive.new_document('Star Wars Episode IV: A New Hope')
 
-    civil_war = archive.new_element(period)
-    episode4.declare_description(civil_war, 'It is a period of civil war.')
+        civil_war = archive.new_element(period)
+        episode4.declare_description(civil_war, 'It is a period of civil war.')
 
-    rebel_victory = archive.new_element(period)
-    episode4.declare_description(
-        rebel_victory,
-        'Rebel spaceships, striking from a hidden base, have won their first'
-        ' victory against the evil Galactic Empire.',
-    )
+        rebel_victory = archive.new_element(period)
+        episode4.declare_description(
+            rebel_victory,
+            'Rebel spaceships, striking from a hidden base, have won their first'
+            ' victory against the evil Galactic Empire.',
+        )
 
-    episode4.declare_order(archive.point(4), archive.point(1))
-    episode4.declare_order(archive.point(2), archive.point(5))
+        episode4.declare_order(archive.point(4), archive.point(1))
+        episode4.declare_order(archive.point(2), archive.point(5))
 
-    archive.analyze_rules()
+        archive.analyze_rules()
 
-    archive.commit()
-    archive.close()
+    finally:
+        archive.commit()
+        archive.close()
