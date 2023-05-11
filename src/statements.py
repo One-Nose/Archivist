4590929f6e84078c7221b145ea2517aea4c4fff5
One Nose <idohaolam@gmail.com>
2023-05-11 20:53:38 +0300

    refactor(statements): change how chaining works
    Statement modification methods now actually modify the object instead of
    creating another one.
    
    Add Statement.__str__() and Statement._get_params().
    Add to DataStatement and Select properties that show its structure.
    Add DataStatement.limit() and DataStatement.order_by().
    Statement modifications can now be applied in any order.

--- src/statements.py
+++ src/statements.py
@@ -33,27 +33,74 @@ class Statement:
         self._params = tuple(params)
 
     def __repr__(self) -> str:
-        return f'{type(self).__name__}({repr(self._statement)})'
+        return f'{type(self).__name__}({repr(str(self))})'
+
+    def __str__(self) -> str:
+        return self._statement
+
+    def _get_params(self) -> tuple[Any, ...]:
+        """
+        :return: the statement's parameters
+        """
+
+        return self._params
 
     def execute(self) -> None:
         """Executes the statement"""
 
-        self._database.execute(self._statement, self._params)
+        self._database.execute(str(self), self._get_params())
 
 
 class DataStatement(Statement):
     """Represents a data manipulation/query statement"""
 
+    _limit: str = ''
+    _limit_count: int | None = None
+    _order_by: str = ''
+    _where: str = ''
+    _where_params: tuple[Any, ...] = ()
+
+    def __str__(self) -> str:
+        return super().__str__() + self._where + self._order_by + self._limit
+
+    def _get_params(self) -> tuple[Any, ...]:
+        return (
+            super()._get_params()
+            + self._where_params
+            + (self._limit_count is not None) * (self._limit_count,)
+        )
+
+    def limit(self, amount: int) -> Self:
+        """
+        Modifies the LIMIT clause of the statement
+        :param amount: The amount of rows to limit to
+        :return: This statement
+        """
+
+        self._limit = f' LIMIT ?'
+        self._limit_count = amount
+        return self
+
+    def order_by(self, column: str) -> Self:
+        """
+        Modifies the statement's ORDER BY clause
+        :param column: The column to order by
+        :return: This statement
+        """
+
+        self._order_by = f' ORDER BY {column}'
+        return self
+
     def where(self, **conditions: Cell[Any] | str | tuple[Cell[Any], ...]) -> Self:
         """
-        Creates a version of the statement with a WHERE clause
+        Modifies the statement's WHERE clause
         :param conditions: The conditions that must be met, in the form of column=value
             value may be either a cell, a string or a tuple of cells to match at least one.
-        :return: The new statement
+        :return: This statement
         """
 
         clauses: list[str] = []
-        params = list(self._params)
+        params: list[Any] = []
 
         for column, value in conditions.items():
             if isinstance(value, tuple):
@@ -65,29 +112,32 @@ class DataStatement(Statement):
             else:
                 clauses.append(f'{column} = {value}')
 
-        return self.__class__(
-            self._database, f'{self._statement} WHERE {" AND ".join(clauses)}', params
-        )
+        self._where = ' WHERE ' + ' AND '.join(clauses)
+        self._where_params = tuple(params)
+
+        return self
 
 
 class Select(DataStatement):
     """Represents a SELECT statement"""
 
+    _into: str = ''
+
+    def __str__(self) -> str:
+        return self._into + super().__str__()
+
     def into(self, table: str, columns: Iterable[str]) -> Statement:
         """
-        Creates an INSERT INTO ... SELECT version of the SELECT statement
+        Turns the statement into a INSERT INTO ... SELECT statement
         :param table: The table to insert the selected values to
         :param columns: A list of columns to insert the selection into
-        :return: An INSERT...SELECT statement
+        :return: This statement
         """
 
-        return Statement(
-            self._database,
-            f'INSERT INTO {table} ({", ".join(columns)}) {self._statement}',
-            self._params,
-        )
+        self._into = f'INSERT INTO {table} ({", ".join(columns)}) '
+        return self
 
     def execute(self) -> list[tuple[Any, ...]]:
         super().execute()
 
-        return self._database.fetch()
+        return [] if self._into else self._database.fetch()
