6174738d26bff8125a5674507777c495e26bc17d
One <idohaolam@gmail.com>
2023-05-22 11:53:58 +0300

    docs(tests): document test_archive.py

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,3 +1,5 @@
+"""Tests `src.archive`"""
+
 from json import load
 
 from pytest import fixture
@@ -8,11 +10,13 @@ from src.archive import Archive, ArchiveConfig
 class TestArchive:
     @fixture(scope='module')
     def config(self):
+        """Returns configurations for the archive"""
         with open('config.json') as file:
             return load(file)
 
     @fixture(scope='module')
     def archive(self, config: ArchiveConfig):
+        """Returns the archive"""
         archive = Archive(config)
         yield archive
         archive.close()
