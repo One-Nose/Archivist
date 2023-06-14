346e64d0b3597719148c86ef4050aeb9da8769e4
One Nose <idohaolam@gmail.com>
2023-06-14 21:45:03 +0300

    refactor(server.client): make client global
    Move logging from main to client.
    Add disconnect().
    Recieve() no longer needs it parameter.
    Add send().
    Disconnect in main.

--- server/client.py
+++ server/client.py
@@ -1,6 +1,6 @@
 """A socket client to connect to the main app"""
 
-from json import loads
+from json import dumps, loads
 from logging import info
 from socket import AF_INET, SOCK_STREAM, gethostname, socket
 from typing import Any
@@ -11,25 +11,46 @@ PORT = 8626
 def connect() -> None:
     """Connects to the archive"""
 
-    with socket(AF_INET, SOCK_STREAM) as client:
-        client.connect((gethostname(), PORT))
-        message = recieve(client)
-        assert message['message'] == 'connection'
-        info('Connected to archive')
-        if message['connected']:
-            info('Connected to database')
-        else:
-            info('Connecting to database...')
-            message = recieve(client)
-            if message['message'] == 'connection' and message['connected']:
-                info('Connected to database')
+    info('Connecting to archive...')
+    client.connect((gethostname(), PORT))
+    message = recieve()
+    assert message['message'] == 'connection'
+    info('Connected to archive')
+    if not message['connected']:
+        info('Connecting to database...')
+        message = recieve()
+        assert message['message'] == 'connection' and message['connected']
+    info('Connected to database')
 
 
-def recieve(client: socket) -> dict[str, Any]:
+def disconnect() -> None:
+    """Closes the connection"""
+
+    info('Disconnecting...')
+    client.close()
+    info('Disconnected')
+
+
+def recieve() -> dict[str, Any]:
     """
     Recieves JSON data
-    :param client: The socket to listen to data
     :return: The data
     """
 
     return loads(client.recv(1024))
+
+
+def send(message: dict[str, Any]) -> dict[str, Any]:
+    """
+    Sends a message to the archive and awaits the response
+    :param message: The message to send
+    :return: The response
+    """
+
+    client.sendall(dumps(message).encode())
+    data = recieve()
+    assert data['message'] == 'response' and data['response'] == message['message']
+    return data
+
+
+client = socket(AF_INET, SOCK_STREAM)
--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 """The main executed file of the server program"""
 
-from logging import INFO, basicConfig, info
+from logging import INFO, basicConfig
 
-from .client import connect
+from .client import connect, disconnect
 
 basicConfig(format='[%(asctime)s] %(message)s', level=INFO, datefmt='%H:%M:%S')
 
-info('Connecting to archive...')
 connect()
+disconnect()
