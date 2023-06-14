34ae204d1ed50ad566a5e5d5789ba675efe1b916
One Nose <idohaolam@gmail.com>
2023-06-14 21:35:16 +0300

    style(blueprints): add blank lines

--- blueprints/index/index.py
+++ blueprints/index/index.py
@@ -1,4 +1,5 @@
 """The website's main page"""
+
 from flask import Blueprint, render_template
 
 index = Blueprint('index', __name__, template_folder='templates')
@@ -7,4 +8,5 @@ index = Blueprint('index', __name__, template_folder='templates')
 @index.route('/')
 def show() -> str:
     """:return: The website's main page HTML"""
+
     return render_template('index.html')
