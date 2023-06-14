7dfd581876bbc20794a0855ecbe17daaec68169e
One Nose <idohaolam@gmail.com>
2023-06-14 21:45:39 +0300

    feat(server.main): start HTTP server

--- server/main.py
+++ server/main.py
@@ -3,8 +3,10 @@
 from logging import INFO, basicConfig
 
 from .client import connect, disconnect
+from .server import start_server
 
 basicConfig(format='[%(asctime)s] %(message)s', level=INFO, datefmt='%H:%M:%S')
 
 connect()
+start_server()
 disconnect()
