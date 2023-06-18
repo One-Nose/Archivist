d415c5d3801201ac19d394bc2f5649a91fcca4e5
One Nose <idohaolam@gmail.com>
2023-06-18 11:37:15 +0300

    fix(server.main): can't connect to docket server
    Replace logging.info with error.

--- server/main.py
+++ server/main.py
@@ -1,19 +1,25 @@
 """The main executed file of the server program"""
 
-from logging import INFO, basicConfig, info
+from logging import INFO, basicConfig, info, error
+
+from socket import gethostbyname, gaierror
 
 from .client import connect, disconnect
 from .server import start_server
 
 basicConfig(format='[%(asctime)s] %(message)s', level=INFO, datefmt='%H:%M:%S')
 
-host = input('Enter host: ')
-key = input('Enter Key: ')
-
 try:
-    connect(host, key)
-except ConnectionRefusedError:
-    info('Could not connect')
+    host = gethostbyname(input('Enter host: '))
+except gaierror:
+    error('Could not find the host.')
 else:
-    start_server()
-    disconnect()
+    key = input('Enter Key: ')
+
+    try:
+        connect(host, key)
+    except ConnectionRefusedError:
+        error('Could not connect')
+    else:
+        start_server()
+        disconnect()
